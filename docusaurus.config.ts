52572965970156aeae87d58a0ff35a627ca605b5
ryukato <ryukato79@gmail.com>
2024-04-03 10:02:29 +0900

    fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://ryukato.gihub.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sample',
+  baseUrl: '/sample-ryukato.github.io',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
